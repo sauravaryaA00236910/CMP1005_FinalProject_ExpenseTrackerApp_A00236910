e339cebd1862d21fb1da0c917b6217463b475f6f
sauravaryaA00236910 <84348791+sauravaryaA00236910@users.noreply.github.com>
2021-12-03 17:00:04 -0500

    Initial Commit Project Setup

---
+++ CMP1005_FinalProject_ExpenseTrackerApp_A00236910/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CMP1005_FinalProject_ExpenseTrackerApp_A00236910.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CMP1005_FinalProject_ExpenseTrackerApp_A00236910/Models/Expense.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CMP1005_FinalProject_ExpenseTrackerApp_A00236910.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string ExpenseTitle { get; set; }
+        public string ExpenseDetails { get; set; }
+        public DateTime DateTime { get; set; }
+
+        public Expense()
+        {
+
+        }
+    }
+}
