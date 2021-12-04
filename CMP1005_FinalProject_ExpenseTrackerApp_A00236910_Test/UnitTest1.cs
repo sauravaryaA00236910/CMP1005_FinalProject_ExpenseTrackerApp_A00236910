d7bdaf657e2ca8f15cd735213da26721344b47d5
sauravaryaA00236910 <84348791+sauravaryaA00236910@users.noreply.github.com>
2021-12-03 20:14:34 -0500

    Unit Test Folder added for testing

---
+++ CMP1005_FinalProject_ExpenseTrackerApp_A00236910_Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CMP1005_FinalProject_ExpenseTrackerApp_A00236910_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
