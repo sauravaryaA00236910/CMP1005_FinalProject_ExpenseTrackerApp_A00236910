87ec22ee744ced5df69c61f003bf032469440776
sauravaryaA00236910 <84348791+sauravaryaA00236910@users.noreply.github.com>
2021-12-03 19:14:25 -0500

    Database migrations added and updated

--- CMP1005_FinalProject_ExpenseTrackerApp_A00236910/Data/Migrations/20211204000752_InitialCreate.Designer.cs
+++ CMP1005_FinalProject_ExpenseTrackerApp_A00236910/Data/Migrations/20211204000752_InitialCreate.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using CMP1005_FinalProject_ExpenseTrackerApp_A00236910.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CMP1005_FinalProject_ExpenseTrackerApp_A00236910.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211204000752_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
