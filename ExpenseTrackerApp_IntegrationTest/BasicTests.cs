6e5978da914fea9a4f4c5a85434fb05fa36850b4
sauravaryaA00236910 <84348791+sauravaryaA00236910@users.noreply.github.com>
2021-12-14 02:33:23 -0500

    Readme.md file added

---
+++ ExpenseTrackerApp_IntegrationTest/BasicTests.cs
@@ -0,0 +1,10 @@
+﻿//using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+namespace ExpenseTrackerApp_IntegrationTest
+{
+    public class BasicTests
+    { 
+    }
+}
