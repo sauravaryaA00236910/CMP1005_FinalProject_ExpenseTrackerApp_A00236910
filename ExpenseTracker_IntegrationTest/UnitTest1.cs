6a93a8df7f311b78b42927a2d4472893ec8971ce
sauravaryaA00236910 <84348791+sauravaryaA00236910@users.noreply.github.com>
2021-12-17 02:25:54 -0500

    Some Integration Test Added to the Expense Tracker Solution

--- ExpenseTracker_IntegrationTest/UnitTest1.cs
+++ ExpenseTracker_IntegrationTest/UnitTest1.cs
@@ -1,13 +1,34 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
 
 namespace ExpenseTracker_IntegrationTest
 {
     [TestClass]
     public class UnitTest1
     {
+        IWebDriver _driver;
+
+        [TestInitialize]
+        public void Startup()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            _driver = new ChromeDriver();
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
+            _driver.Navigate().GoToUrl("https://localhost:");
+            Assert.AreEqual("Expense Tracker", _driver.Title);
+        }
+
+        [TestCleanup]
+        public void Shutdown()
+        {
+            _driver.Quit();
         }
     }
 }
