bafa782eefbc6fd922e8cb640b794901c51cfaef
sauravaryaA00236910 <84348791+sauravaryaA00236910@users.noreply.github.com>
2021-12-17 20:57:36 -0500

    Some more Integration Tests are added for more screens and pages

--- ExpenseTracker_IntegrationTest/UnitTest1.cs
+++ ExpenseTracker_IntegrationTest/UnitTest1.cs
@@ -19,11 +19,43 @@ namespace ExpenseTracker_IntegrationTest
         }
 
         [TestMethod]
-        public void TestTitle()
+        public void TestHomeTitle()
         {
             _driver.Navigate().GoToUrl("https://localhost:5001/");
             Assert.AreEqual("Expense Tracker", _driver.Title);
             Assert.IsTrue(_driver.Title.Contains("Expense Tracker"));
+        }
+
+        [TestMethod]
+        public void TestExpenseTitle()
+        {
+            _driver.Navigate().GoToUrl("https://localhost:5001/Expenses");
+            Assert.AreEqual("Index", _driver.Title);
+            Assert.IsTrue(_driver.Title.Contains("Index"));
+        }
+
+        [TestMethod]
+        public void TestCreateTitle()
+        {
+            _driver.Navigate().GoToUrl("https://localhost:5001/Expenses/Create");
+            Assert.AreEqual("Create", _driver.Title);
+            Assert.IsTrue(_driver.Title.Contains("Create"));
+        }
+
+        [TestMethod]
+        public void TestLoginTitle()
+        {
+            _driver.Navigate().GoToUrl("https://localhost:5001/Identity/Account/Login");
+            Assert.AreEqual("Log in", _driver.Title);
+            Assert.IsTrue(_driver.Title.Contains("Log in"));
+        }
+
+        [TestMethod]
+        public void TestRegisterTitle()
+        {
+            _driver.Navigate().GoToUrl("https://localhost:5001/Identity/Account/Register");
+            Assert.AreEqual("Register", _driver.Title);
+            Assert.IsTrue(_driver.Title.Contains("Register"));
         }
 
         [TestCleanup]
